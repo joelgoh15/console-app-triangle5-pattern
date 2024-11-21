899956fe4d89d5ab24b540f0d27394e9c6093af9
Joel Goh Chong Hui <147377225+joelgoh15@users.noreply.github.com>
2024-11-21 22:02:13 +0800

    upload project files.

---
+++ ClassPatternTriangle5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio10_4
+{
+    internal class ClassPatternTriangle5
+    {
+        public static void DramTriangle5Pattern(int sizeTriangle5Pattern)
+        {
+            int midSizeTriangle5Pattern;
+            midSizeTriangle5Pattern = (sizeTriangle5Pattern / 2) + 1;
+            int spaceCounter;
+            spaceCounter = midSizeTriangle5Pattern - 1;
+            int plusMinusCounter = 1;
+            for (int i = 0; i <= sizeTriangle5Pattern; i++)
+            {
+                if (i < midSizeTriangle5Pattern)
+                {
+                    for (int j = 0; j < spaceCounter; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    spaceCounter = spaceCounter - 1;
+                    for (int k = 0; k < plusMinusCounter; k++)
+                    {
+                        if (k % 2 == 0)
+                        {
+                            Console.Write("+");
+                        } 
+                        else 
+                        {
+                            Console.Write("-");
+                        }
+                    }
+                    plusMinusCounter = plusMinusCounter + 2;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio10_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome.");
+            Console.WriteLine("A console application that draws the triangle5 pattern base on user input value.");
+            Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+            string strSizeTriangle5Pattern;
+            int sizeTriangle5Pattern;
+            sizeTriangle5Pattern = 0;
+            do
+            {
+                Console.Write("Please enter size of triangle5 pattern (enter -1 to exit):");
+                try
+                {
+                    strSizeTriangle5Pattern = Console.ReadLine();
+                    strSizeTriangle5Pattern = strSizeTriangle5Pattern.Trim();
+                    sizeTriangle5Pattern = Convert.ToInt16(strSizeTriangle5Pattern);
+                    if ((sizeTriangle5Pattern % 2) == 0)
+                    {
+                        Console.WriteLine("enter only odd integer value for size of triangle5 pattern.");
+                        continue;
+                    }
+                    if (sizeTriangle5Pattern < 3)
+                    {
+                        Console.WriteLine("please enter value that is more than 3.");
+                        continue;
+                    }
+                }
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine("please enter a valid input. please enter an integer.");
+                    continue;
+                }
+                ClassPatternTriangle5.DramTriangle5Pattern(sizeTriangle5Pattern);
+                Console.Write("\n\n");
+            } while (sizeTriangle5Pattern != -1);
+            Console.WriteLine("program exit.");
+            Console.WriteLine("press any button to exit.");
+            Console.ReadLine();
+        }
+    }
+}
